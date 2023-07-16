696369de4eb046ff6c102f58358c8ce89ee458c7
Bonolo Cynthia Selemale <139655743+CynthiaSelemale@users.noreply.github.com>
2023-07-16 21:22:51 +0200

    Add files via upload

---
+++ GymMembershipFrameApp.java
@@ -0,0 +1,16 @@
+
+package gymmembershipframeapp;
+
+import za.ac.tut.ui.GymMembershipFrame;
+
+/**
+ *
+ * @author cynthia
+ */
+public class GymMembershipFrameApp {
+
+    public static void main(String[] args) {
+        new GymMembershipFrame();
+    }
+    
+}
